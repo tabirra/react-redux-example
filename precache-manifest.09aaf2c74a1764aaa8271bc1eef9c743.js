61f7963051a909b2583864cc4df4068d97eea257
Miguel Gomez-Tavira <gomeztavira.miguel@gmail.com>
2019-03-06 18:57:27 +0100

    Updates

---
+++ precache-manifest.09aaf2c74a1764aaa8271bc1eef9c743.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f71c0a2baf7248d9dc",
+    "url": "/tabirra/react-redux-example/static/js/runtime~main.79f71c0a.js"
+  },
+  {
+    "revision": "574e1dc915050c26411c",
+    "url": "/tabirra/react-redux-example/static/js/main.574e1dc9.chunk.js"
+  },
+  {
+    "revision": "5158b892a7c7b78ea871",
+    "url": "/tabirra/react-redux-example/static/js/2.5158b892.chunk.js"
+  },
+  {
+    "revision": "574e1dc915050c26411c",
+    "url": "/tabirra/react-redux-example/static/css/main.5fa85e70.chunk.css"
+  },
+  {
+    "revision": "a759312f7589041eb90bf20a8b0a706e",
+    "url": "/tabirra/react-redux-example/index.html"
+  }
+];
